fb253cc7ddad05d505502b6037fd30ba6b9dc92d
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-11-30 21:25:34 +0300

    feat: add action handler for compare module

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import compare from "../src/compare.js";
+
 const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format')
-    .parse();
+    .action((filepath1, filepath2) => {
+        console.log(compare(filepath1, filepath2, program.opts().format))
+    });
 
-
+program.parse();
 
 
