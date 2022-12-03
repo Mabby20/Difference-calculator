d85b707e71f54cd4eb067d73e9ef785a72e06170
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-12-03 14:17:22 +0300

    refactor: fix code style

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import compare from "../src/compare.js";
+import compare from '../src/compare.js';
 
 const program = new Command();
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2) => {
-        console.log(compare(filepath1, filepath2, program.opts().format))
-    });
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(compare(filepath1, filepath2, program.opts().format));
+  });
 
 program.parse();
-
-
