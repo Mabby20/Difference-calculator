2c052a789b3660a1760a3427e82a89c1c7cf2b8d
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-12-03 14:23:28 +0300

    fix: fix mutability of the main function

--- src/compare.js
+++ src/compare.js
@@ -1,37 +1,38 @@
-import fs from "fs";
-import path from "path";
-import _ from "lodash";
+import path from 'path';
+import _ from 'lodash';
+import parser from './parser.js';
 
 const compare = (obj1, obj2) => {
-    const commonKeys = [...Object.keys(obj1), ...Object.keys(obj2)];
-    const uniqKeys = _.sortBy(_.uniq(commonKeys));
+  const commonKeys = [...Object.keys(obj1), ...Object.keys(obj2)];
+  const uniqKeys = _.sortBy(_.uniq(commonKeys));
 
-    const lines = uniqKeys.reduce((acc, currentKey) => {
-        if (_.has(obj1, currentKey) && _.has(obj2, currentKey)) {
-            if (obj2[currentKey] === obj1[currentKey]) {
-                return [...acc, `    ${currentKey}: ${obj1[currentKey]}`]
-            }
-            return [...acc, `  - ${currentKey}: ${obj1[currentKey]}`, `  + ${currentKey}: ${obj2[currentKey]}`]
-        }
-        if(_.has(obj1, currentKey) && !_.has(obj2, currentKey)) {
-            return [...acc, `  - ${currentKey}: ${obj1[currentKey]}`]
-        }
-        if(!_.has(obj1, currentKey) && _.has(obj2, currentKey)) {
-            return [...acc, `  + ${currentKey}: ${obj2[currentKey]}`]
-        }
-    }, []);
+  const lines = uniqKeys.reduce((acc, currentKey) => {
+    if (_.has(obj1, currentKey) && _.has(obj2, currentKey)) {
+      if (obj2[currentKey] === obj1[currentKey]) {
+        return [...acc, `    ${currentKey}: ${obj1[currentKey]}`];
+      }
+      return [...acc, `  - ${currentKey}: ${obj1[currentKey]}`, `  + ${currentKey}: ${obj2[currentKey]}`];
+    }
+    if (_.has(obj1, currentKey) && !_.has(obj2, currentKey)) {
+      return [...acc, `  - ${currentKey}: ${obj1[currentKey]}`];
+    }
+    if (!_.has(obj1, currentKey) && _.has(obj2, currentKey)) {
+      return [...acc, `  + ${currentKey}: ${obj2[currentKey]}`];
+    }
+    return acc;
+  }, []);
 
-    const res = ['{', ...lines, '}'].join('\n');
-    return res;
-}
+  const res = ['{', ...lines, '}'].join('\n');
+  return res;
+};
+
+const getAbsPath = (filePath) => path.resolve(process.cwd(), filePath);
 
 export default (filePath1, filePath2, type = 'json') => {
-    let obj1;
-    let obj2;
-    if (type === "json") {
-        obj1 = JSON.parse(fs.readFileSync(path.resolve(filePath1)));
-        obj2 = JSON.parse(fs.readFileSync(path.resolve(filePath2)));
-    }
-    const res = compare(obj1, obj2);
-    return res;
+  const path1 = getAbsPath(filePath1);
+  const path2 = getAbsPath(filePath2);
+  const obj1 = parser(type, path1);
+  const obj2 = parser(type, path2);
+  const res = compare(obj1, obj2);
+  return res;
 };
