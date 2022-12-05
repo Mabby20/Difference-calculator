f6122283c1f1375d8738662173fa4ed26f7b25fc
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-12-05 14:47:33 +0300

    test: add new tests for yaml and yml files

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -7,11 +7,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
+const data = [
+  ['file1.json', 'file2.json', 'stylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'stylish.txt'],
+  ['file1.yml', 'file2.yml','stylish.txt'],
+]
 
 describe('check compare', () => {
-  test('Stylish', () => {
-    const expectedStylish = readFixture('stylish.txt').trim();
-    const actual = compare(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
-    expect(actual).toBe(expectedStylish);
-  });
+  test.each(data)(`compare test %s, %s, %s`, (firstFile, secondFile, stylishFile) => {
+    const expectedResult = readFixture(stylishFile).trim();
+    const actualResult = compare(getFixturePath(firstFile), getFixturePath(secondFile))
+    expect(actualResult).toBe(expectedResult);
+  })
 });
