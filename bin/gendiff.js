783ff6edc6ee91d78139280be18d49b563ac51af
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-12-09 11:52:16 +0300

    feat: add the default uotput format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import compare from '../src/compare.js';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(compare(filepath1, filepath2, program.opts().format));
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
