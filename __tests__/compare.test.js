542e65719f79fb7f22a9fada1f309587fdbd249e
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-12-09 11:43:59 +0300

    test: update test for nested structures

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -1,22 +1,22 @@
 import fs from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import compare from '../src/compare.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
-const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
-const data = [
-  ['file1.json', 'file2.json', 'stylish.txt'],
-  ['file1.yaml', 'file2.yaml', 'stylish.txt'],
-  ['file1.yml', 'file2.yml', 'stylish.txt'],
+const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const data2 = [
+  ['file1.json', 'file2.json', 'expected-stylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'expected-stylish.txt'],
 ];
 
 describe('check compare', () => {
-  test.each(data)('compare test %s, %s, %s', (firstFile, secondFile, stylishFile) => {
+  test.each(data2)('compare test nested %s, %s, %s', (firstFile, secondFile, stylishFile) => {
     const expectedResult = readFixture(stylishFile).trim();
-    const actualResult = compare(getFixturePath(firstFile), getFixturePath(secondFile));
+    const actualResult = genDiff(getFixturePath(firstFile), getFixturePath(secondFile));
     expect(actualResult).toBe(expectedResult);
   });
 });
