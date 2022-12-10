233e16dc0a72fe90d90063d226f2805e469bac00
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-12-10 21:05:39 +0300

    test: add new tests for plain output

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -9,14 +9,16 @@ const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filen
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const data2 = [
-  ['file1.json', 'file2.json', 'expected-stylish.txt'],
-  ['file1.yaml', 'file2.yaml', 'expected-stylish.txt'],
+  ['file1.json', 'file2.json', 'expected-stylish.txt', 'stylish'],
+  ['file1.yaml', 'file2.yaml', 'expected-stylish.txt', 'stylish'],
+  ['file1.json', 'file2.json', 'expected-plain.txt', 'plain'],
+  ['file1.yaml', 'file2.yaml', 'expected-plain.txt', 'plain'],
 ];
 
 describe('check compare', () => {
-  test.each(data2)('compare test nested %s, %s, %s', (firstFile, secondFile, stylishFile) => {
+  test.each(data2)('compare test nested %s, %s, %s', (firstFile, secondFile, stylishFile, formatter) => {
     const expectedResult = readFixture(stylishFile).trim();
-    const actualResult = genDiff(getFixturePath(firstFile), getFixturePath(secondFile));
+    const actualResult = genDiff(getFixturePath(firstFile), getFixturePath(secondFile), formatter);
     expect(actualResult).toBe(expectedResult);
   });
 });
