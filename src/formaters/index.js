ca1bc853caaaa146aa933dc95506337f887fd4db
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-12-10 23:25:46 +0300

    feat: add json formater for output

--- src/formaters/index.js
+++ src/formaters/index.js
@@ -7,6 +7,8 @@ export default (data, outputFormat) => {
       return stylish(data);
     case 'plain':
       return plain(data);
+    case 'json':
+      return JSON.stringify(data);
     default:
       throw new Error(`This ${outputFormat} not support`);
   }
