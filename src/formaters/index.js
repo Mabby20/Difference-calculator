c059c9a378cd093b818f0275b2816bbaf8a3cecd
Kolotiy Pavel <kolotiy199420@mail.ru>
2022-12-10 21:09:19 +0300

    feat: add plain formatter for output

--- src/formaters/index.js
+++ src/formaters/index.js
@@ -1,9 +1,12 @@
 import stylish from './stylish.js';
+import plain from './plain.js';
 
 export default (data, outputFormat) => {
   switch (outputFormat) {
     case 'stylish':
       return stylish(data);
+    case 'plain':
+      return plain(data);
     default:
       throw new Error(`This ${outputFormat} not support`);
   }
